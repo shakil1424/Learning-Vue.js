97498ab72f1046bc12cc2697c050186423b07aca
SHAKIL-UDDIN-DE\Shakil Uddin <shakiluddin@ssd-tech.io>
2022-08-25 20:55:50 +0600

    Event modifiy default behaviour is understood

--- basics-events-starting-code/app.js
+++ basics-events-starting-code/app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmed_name: '',
     };
   },
   methods: {
+    submitForm() {
+      alert('Form tried to be submitted');
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    confirmName() {
+      this.confirmed_name = this.name;
+    },
     increment(num) {
       this.counter = this.counter + num;
     },
