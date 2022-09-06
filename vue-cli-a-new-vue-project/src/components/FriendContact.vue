7a671aebdafd8b0abb4d36a4aca439670314c6fa
SHAKIL-UDDIN-DE\Shakil Uddin <shakiluddin@ssd-tech.io>
2022-09-06 15:35:18 +0600

    Basic understanding of project structure usimg vue cli is done

---
+++ vue-cli-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.name }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'shakil',
+        name: 'Shakil',
+        phone: '01307156557',
+        email: 'shakil1424@gmail.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+<style lang=""></style>
