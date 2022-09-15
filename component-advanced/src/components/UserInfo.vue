109192ad19c52198698a7cfd0395cd77028d7695
SHAKIL-UDDIN-DE\Shakil Uddin <shakiluddin@ssd-tech.io>
2022-09-15 18:30:23 +0600

    Use of slot is understood

---
+++ component-advanced/src/components/UserInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-card>
+    <!-- <template v-slot:header> -->
+    <!-- Short hand for v-slot -->
+    <template #header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </template>
+    <template #default>
+      <p>{{ infoText }}</p>
+    </template>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped></style>
