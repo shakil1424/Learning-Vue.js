f933573ef365fea73572865869c60d653032c0b3
SHAKIL-UDDIN-DE\Shakil Uddin <shakiluddin@ssd-tech.io>
2022-09-20 11:19:19 +0600

    Scoped slot is completed

--- component-advanced/src/App.vue
+++ component-advanced/src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>
+          <h4>{{ slotProps.item }}</h4>
+          <p>{{ slotProps['extra'] }}</p>
+        </h2>
+      </template>
+    </course-goals> -->
+    <button>Active Goals</button>
+    <button>Manage Goals</button>
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
+/* import UserInfo from './components/UserInfo.vue';
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue'; */
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+
 export default {
   components: {
     TheHeader,
-    UserInfo,
+    /*  UserInfo,
     BadgeList,
+    CourseGoals, */
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
